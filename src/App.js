be33def1ab563f3cff983a547e005a0c015db121
Greg <greg_markarian@hotmail.com>
2021-08-23 12:45:33 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return <div>
-    <h1>hello</h1>
+    <Header />
   </div>;
 }
 
