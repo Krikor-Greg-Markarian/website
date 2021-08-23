b238fbecabf97c5059cc480b7812775178d5006a
Greg <greg_markarian@hotmail.com>
2021-08-23 15:11:41 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import InputDesButton from "./components/InputDesButton";
 
 function App() {
-  return <div>
-    <Header />
-  </div>;
+  return (
+    <div>
+      <div className={"house-image"}>
+        <p className="text-white font-bold p-2">Houzing</p>
+        <li className="list-none float-right ">
+          <a className="text-white font-bold p-3" href="">
+            HOME
+          </a>
+          <a className="text-white font-bold p-3" href="">
+            LISTING
+          </a>
+          <a className="text-white font-bold p-3" href="">
+            PROPERTY
+          </a>
+          <a className="text-white font-bold p-3" href="">
+            PAGES
+          </a>
+          <a className="text-white font-bold p-3" href="">
+            CONTACT
+          </a>
+          <a className="text-white font-bold p-3" href="">
+            (+88 1990 6836)
+          </a>
+        </li>
+        <br></br>
+        <br></br>
+        <div className="container mx-auto">
+          <div className="font-bold text-white text-center">
+            IT'S GREAT TO BE HOME
+          </div>
+          <p className="text-white text-2xl text-center font-bold">
+            Find Your Perfect Home
+          </p>
+          <li className="list-none text-center mt-4">
+            <a className="text-white font-bold p-4" href="#">
+              BUY
+            </a>
+            <a className="text-white font-bold p-4 underline " href="#">
+              RENT
+            </a>
+            <a className="text-white font-bold p-4" href="#">
+              SOLD
+            </a>
+          </li>
+
+          <InputDesButton />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/InputDesButton.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function InputDesButton() {
+    return (
+    
+        <div className="grid grid-cols-5 border-2 border-gray-300 bg-white p-1 mt-8 flex">
+          <div className="flex flex-col">
+            <label className="text-black flex justify-center" htmlFor="">
+              TYPE
+            </label>
+            <input
+              className="border-2 text-center border-none focus: outline-none"
+              placeholder="Property Types"
+              type="text"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-black flex justify-center" htmlFor="">
+              LOCATION
+            </label>
+            <input
+              className="border-2 border-none text-center focus: outline-none"
+              placeholder="All Cities"
+              type="text"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="text-black flex justify-center" htmlFor="">
+              SEARCH
+            </label>
+            <input
+              className="border-2 border-none text-center focus: outline-none"
+              placeholder="Enter Keywords"
+              type="text"
+            />
+          </div>
+          <div></div>
+
+          <button className="bg-blue-700 text-white">SEARCH</button>
+        </div>
+      
+    );
+}
+
+export default InputDesButton
