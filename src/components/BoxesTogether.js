5c9a7fca7c371e58a4f4850edae682bd4cb2a68c
Greg <greg_markarian@hotmail.com>
2021-08-24 11:20:36 +0300

    Grid Boxes Form

--- src/components/BoxesTogether.js
+++ src/components/BoxesTogether.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function BoxesTogether(props) {
   return (
-    <div className="bg-blue-400 w-32 h-32 flex justify-center items-center">
+    <div className="bg-blue-500 w-32 h-32 flex justify-center items-center">
       {props.name}
     </div>
   );
