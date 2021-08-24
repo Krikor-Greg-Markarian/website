3eaf469365e225102d811096c7f3c677037b562e
Greg <greg_markarian@hotmail.com>
2021-08-24 11:28:21 +0300

    profile image with names and positions

--- src/components/ImageWBoxName.js
+++ src/components/ImageWBoxName.js
@@ -5,7 +5,7 @@ function ImageWBoxName(props) {
       <div className="border-2 p-3">
         <div className={"nature"}></div>
 
-        <div className="border-2 text-center rounded-lg -mt-6 bg-white">
+        <div className="border-3 text-center rounded-lg -mt-6 bg-white">
           <p className="">{props.name}</p>
           <p className="">{props.position}</p>
         </div>
