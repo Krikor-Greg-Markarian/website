10cab832cf9869187d23f7061e074f65114bd6a5
Greg <greg_markarian@hotmail.com>
2021-08-30 12:36:13 +0300

    images and text inside a box.

--- src/components/BoxesWithText.js
+++ src/components/BoxesWithText.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function BoxesWithText() {
-    return (
-      <div className="bg-white p-2">
-        <div className={"villa"}></div>
-        <p className="font-bold mt-2 text-center">Trusted By Thousands</p>
-        <p className="mt-2 text-center">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elitee.
-        </p>
-      </div>
-    );
+  return (
+    <div className="bg-white p-2">
+      <div className={"villa"}></div>
+      <p className="font-bold mt-2 text-center">Trusted By Thousands</p>
+      <p className="mt-2 text-center">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elitee.
+      </p>
+    </div>
+  );
 }
 
-export default BoxesWithText
+export default BoxesWithText;
