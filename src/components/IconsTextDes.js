05ee9838356cad9b72ce80ab15bb79a7a050b94e
Greg <greg_markarian@hotmail.com>
2021-08-30 12:36:43 +0300

    Images and texts.

--- src/components/IconsTextDes.js
+++ src/components/IconsTextDes.js
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function IconsTextDes() {
-    return (
-      <div className="bg-white p-2">
-        <div className={"villa"}></div>
-        <p className="font-bold mt-2 text-center">Trusted By Thousands</p>
-        <p className="mt-2 text-center">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elite.
-        </p>
-      </div>
-    );
+function IconsTextDes(props) {
+  return (
+    <div className="bg-white p-2">
+      <div className={"villa"}></div>
+      <p className="font-bold mt-2 text-center">{props.title}</p>
+      <p className="mt-2 text-center">{props.description}</p>
+    </div>
+  );
 }
 
-export default IconsTextDes
+export default IconsTextDes;
