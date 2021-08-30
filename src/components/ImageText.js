46fc9421e6848e4fe8da48d6b82c3d890c865da9
Greg <greg_markarian@hotmail.com>
2021-08-30 13:00:27 +0300

    Add: position box inc. text img name position.

--- src/components/ImageText.js
+++ src/components/ImageText.js
@@ -8,7 +8,7 @@ function ImageText(props) {
         alt="villa"
       /> */}
       <div className={"backgroundappartment"}></div>
-      <div className=" p-2 font-bold">{props.title3}New Apartment Nice View</div>
+      <div className=" p-2 font-bold">{props.title3}New Apartment, Nice View</div>
       <p className=" p-2 border-3 border-b">{props.subtitle3}Quincy St. Brooklyn NY, USA.</p>
       <p className="inline-block">{props.beds}</p>
       <p className="inline-block ml-3">{props.baths}</p>
